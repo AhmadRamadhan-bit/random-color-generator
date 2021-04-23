d8d2532285678bda12266bf6615371f8f5575e60
Ahmad Ramadhan <ahmadoneperson@gmail.com>
2021-04-23 14:39:48 +0700

    Commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const getNewColor = () => {
+    let symbols, color;
+
+    symbols = "0123456789ABCDEF";
+    color = "#";
+
+    for (let i = 0; i < 6; i++) {
+        color += symbols[Math.floor(Math.random() * 16)];
+    }
+    document.body.style.background = color;
+    document.getElementById('hex').innerHTML = color;
+}
+
+const button = document.querySelector('.btn');
+
+
+button.addEventListener('click', getNewColor);
